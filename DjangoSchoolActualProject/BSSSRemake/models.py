154c921d9a5a2a1326e23a26c1dc6ab224f10cd5
Aakash Jai <Aakash.jai@outlook.com>
2026-03-07 17:58:53 +1100

    Added a admin.url, the username is aakash and password is 1234. Also added models with constratints and models are 3NF.

--- DjangoSchoolActualProject/BSSSRemake/models.py
+++ DjangoSchoolActualProject/BSSSRemake/models.py
@@ -1,3 +1,25 @@
+from enum import unique
 from django.db import models
 
 # Create your models here.
+
+
+class School(models.Model):
+    name = models.CharField(max_length = 50) #arbitrarty amount
+    #Human readable values
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    achievementStandards = models.CharField(max_length = 50) #arbitrarty amount
+    #Human readable values
+    def __str__(self):
+        return self.achievementStandards
+
+#Junction Table
+class schoolCourses(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+        #constraints to prevent duplicates
+    class Meta:
+        unique_together = ("school", "course")
--- DjangoSchoolActualProject/DjangoSchoolActualProject/urls.py
+++ DjangoSchoolActualProject/DjangoSchoolActualProject/urls.py
@@ -1,10 +1,12 @@
 #
 #This is a project level urls.py
 #
+from django.contrib import admin
 
 from django.urls import include, path
 import BSSSRemake.views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('BSSSRemake.urls'))
 ]
