5e2d6071d1f287a998e4365f840bc35e63491037
Aakash Jai <Aakash.jai@outlook.com>
2026-03-08 14:00:23 +1100

    added indexing

--- DjangoSchoolActualProject/BSSSRemake/models.py
+++ DjangoSchoolActualProject/BSSSRemake/models.py
@@ -3,19 +3,30 @@ from django.db import models
 # Create your models here.
 
 
-
-
 class Course(models.Model):
-    name = models.CharField(max_length=50)  # Name of the course
+    name = models.CharField(max_length=50, db_index = True)  # Name of the course , Indexed
+
     #Human readable values
     def __str__(self):
         return self.name
+
+    class Meta:
+        indexes = [
+            models.Index(fields = ["name"], name = "courseIndex")
+            ]
+ 
 
 
 #defined course after
 class School(models.Model):
-    name = models.CharField(max_length=50)  # Name of the school
-    courses = models.ManyToManyField(Course, blank=True)  # link to courses
+    name = models.CharField(max_length=50, db_index = True)  # Name of the school, Indexed
+    courses = models.ManyToManyField(Course, blank=True)  # Many to Many link
+
     #Human readable values
     def __str__(self):
         return self.name
+
+    class Meta:
+        indexes = [
+            models.Index(fields = ["name"], name = "schoolIndex")
+            ]
