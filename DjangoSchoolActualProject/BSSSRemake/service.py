beb2e3f9432d5bf8d4005e790f558dc04121a808
Aakash Jai <Aakash.jai@outlook.com>
2026-03-11 13:45:39 +1100

    Added a lot of validation code however the validation still needs to be done usign wrappers. Need to log validations as well, send it to the views and display it in hte html.

--- DjangoSchoolActualProject/BSSSRemake/service.py
+++ DjangoSchoolActualProject/BSSSRemake/service.py
@@ -1,38 +1,92 @@
+from django.forms import formset_factory
 from .models import Course
-from collections import defaultdict
+
+class CourseValidator():
+    def __init__(self, Name, Semester, Unit):
+        self.Name = Name
+        self.Semester = Semester
+        self.Unit = Unit
+    #________________
+    #validation rules
+    #________________
+
+    def duplicate(self):
+        #make coursename = form input
+        if Course.objects.filter(name__iexact = self.name).exists():
+            return f"course '{self.Name}' exists"
+
+    def semOutOfBounds(self):
+        if self.Semester < 0 or self.Semester > 4:
+            return "Semester is out of bounds"
+
+    def UInfoTooShort(self):
+        letterCount = sum(c.isaplha() for c in self.Unit)
+        if letterCount < 5:
+            return "unit info too short"
+
+    #________________
+    #validation rules activation
+    #________________
+
+    def validationRun(self):
+        VRule = [
+            self.semOutOfBounds,
+            self.UInfoTooShort,
+            self.duplicate
+            ]
+        for i in Vrule:
+            #store the return value
+            error = i()
+            #chcek if error exists
+            if error:
+                return error
+
+    def Validate(self):
+        #add validation function 
+        pass
 
 
 
 
 
-class ConflictService():
 
-    def buildAdjacencyList():
 
-            allCourse = Course.objects.all()
-            ID = [course.pk for course in allCourse]  #list comphrehsnion
-            Semesters = [course.semester1 for course in allCourse] #list comphrehsnion
 
-            #Dict comphrehension to zip them together
-            IdNames = {k:v for (k,v) in zip(ID, Semesters)}
 
-            #group by semseers
-            semsterGroup= defaultdict(list)
 
-            for course in allCourse:
-                semesterGroup = [course.semester1].append(course.pk)
+
+
+
+
+
+# class ConflictService():
+
+#     def buildAdjacencyList():
+
+#             allCourse = Course.objects.all()
+#             ID = [course.pk for course in allCourse]  #list comphrehsnion
+#             Semesters = [course.semester1 for course in allCourse] #list comphrehsnion
+
+#             #Dict comphrehension to zip them together
+#             IdNames = {k:v for (k,v) in zip(ID, Semesters)}
+
+#             #group by semseers
+#             semsterGroup= defaultdict(list)
+
+#             for course in allCourse:
+#                 semesterGroup = [course.semester1].append(course.pk)
 
             
-            adjacency = defaultdict(list)
+#             adjacency = defaultdict(list)
         
-            for semester, courseID in semesterGroup.items(): #for primarykey, value in semstergroup
-                for currentCourse in courseID:
-                    #add all courses in the semester as conflicts
-                    conflicts = [
-                        c for c in courseID if c != currentCourse
-                    ]
-                    adjacency[currentCourse].append(conflicts)
+#             for semester, courseID in semesterGroup.items(): #for primarykey, value in semstergroup
+#                 for currentCourse in courseID:
+#                     #add all courses in the semester as conflicts
+#                     conflicts = [
+#                         c for c in courseID if c != currentCourse
+#                     ]
+#                     adjacency[currentCourse].append(conflicts)
                     
 
-            return dict(adjacency)
+#             return dict(adjacency)
     
