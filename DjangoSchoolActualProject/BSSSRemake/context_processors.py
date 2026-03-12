4fcf9004c67e5f2322ac135cf740b3a34ba0267c
Aakash Jai <Aakash.jai@outlook.com>
2026-03-12 14:19:07 +1100

    Added Hyperlinks for views

--- DjangoSchoolActualProject/BSSSRemake/context_processors.py
+++ DjangoSchoolActualProject/BSSSRemake/context_processors.py
@@ -3,5 +3,7 @@
 def customProc(request):
     #This fethces global data in this case being title and content from the template
     return{
-        "projectName": 'BSSS Rework'
+        "projectName": 'BSSS Rework',
+        "LView1": "View1",
+        "LView2": "View2"
         }
