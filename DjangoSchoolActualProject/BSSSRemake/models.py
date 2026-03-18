f569252d4ad5f26d78df031b3f0667f5aa39bfaa
Aakash Jai <Aakash.jai@outlook.com>
2026-03-18 19:36:54 +1100

    Fied html displaying twice final commit for real for the prototype

--- DjangoSchoolActualProject/BSSSRemake/models.py
+++ DjangoSchoolActualProject/BSSSRemake/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=50, db_index=True)
-    semester1 = models.IntegerField(default=0)
+    semester1 = models.IntegerField(default=0, db_index = True)
     unitInformation = models.CharField(max_length=50, db_index=True, default="Unknown")
 
     
